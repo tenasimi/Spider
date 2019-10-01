6f92098910242e87ac30b9da7f56ae8a7179942d
Nasimi Taghizade <tenasimi@gmail.com>
2019-10-01 22:21:41 +0400

    TODO edited with code

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,13 +1,22 @@
+from urllib.request import urlopen
+
+
 def load_urls_from_file(file_path: str):
-    # TODO: add the code needed to read the text file with the urls in it
-    pass
+    try:
+        with open(file_path) as f:
+            content = f.readline()
+            return content
+    except FileNotFoundError:
+        print("the file " + file_path + " could not be found")
+        exit(2)
 
 
 def load_page(url: str):
-    # TODO: add the code that reads the url
-    pass
+    response = urlopen(url)
+    html = response.read().decode('utf-8')
+    return html
 
 
 def scrape_page(page_contents: str):
-    # TODO: analise the text
+    # TODO:  analyze the text
     pass
