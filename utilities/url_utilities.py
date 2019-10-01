dbe5404187feb724df350db5cf6cb80108089b49
Nasimi Taghizade <tenasimi@gmail.com>
2019-10-02 03:05:52 +0400

    adding the scrape-page function

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,4 +1,9 @@
+import re
+import string
 from urllib.request import urlopen
+
+# add the code needed to read the text file (with the urls in it)
+from bs4 import BeautifulSoup  # Alt + Enter  bu setiri yazacaq bura
 
 
 def load_urls_from_file(file_path: str):
@@ -8,15 +13,57 @@ def load_urls_from_file(file_path: str):
             return content
     except FileNotFoundError:
         print("the file " + file_path + " could not be found")
-        exit(2)
+        exit(2)  # zadaem return code unsuccessful
 
 
+# add the code that reads the url
 def load_page(url: str):
     response = urlopen(url)
     html = response.read().decode('utf-8')
     return html
 
 
-def scrape_page(page_contents: str):
-    # TODO:  analyze the text
-    pass
+# analyze the text
+def scrape_page(page_contents: str):  # beautifulSoup screen-scraping library istifade etdim
+    global word
+    chicken_noodle = BeautifulSoup(page_contents, "html5lib")
+
+    for script in chicken_noodle(["script", "style"]):
+        script.extract()
+
+    text = chicken_noodle.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+
+    text = ' '.join(chunk for chunk in chunks if chunk)
+    plain_text = ''.join(filter(lambda x: x in string.printable, text))
+
+    clean_words = []
+
+    words = plain_text.split(" ")
+    for word in words:
+        clean = True
+
+        # no punctuation
+        for punc in string.punctuation:
+            if punc in word:
+                clean = False
+
+                # no numbers
+            if any(char.isdigit() for char in word):
+                clean = False
+
+                # at least two characters but no more than 10
+            if len(word) < 2 or len(word) > 10:
+                clean = False
+
+            if not re.match(r'^\w+$', word):
+                clean = False
+
+            if clean:
+                try:
+                    clean_words.append(word.lower())
+                except UnicodeEncodeError:
+                    print(".")
+
+        return clean_words
